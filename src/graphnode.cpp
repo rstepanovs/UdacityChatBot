e2ce6acb1b44fcc8fd799a12e95eda96ad9e8017
Romans.Stepanovs <v-romste@microsoft.com>
2025-08-17 15:02:38 +0200

    WarmUp task: Fixed crash

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // ChatLogic creates the object and should delete it.
+    //delete _chatBot;
 
     ////
     //// EOF STUDENT CODE
